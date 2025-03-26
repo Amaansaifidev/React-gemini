8b4ecae0023c5653e48c29cc2076164ae688579c
Amaan-saifidev <amaansaifidev9@gmail.com>
2025-03-27 01:09:38 +0530

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import axios from 'axios'
+import { useState } from 'react'
 
+
+
+
+
+const App = () => {
+  
+  const [answer, setAnswer] = useState("loading....")
 const handlePostRequest = async () => {
     try {
       const response = await axios.post("https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=AIzaSyCzSc8Vi_MfwfqLUz2Veg7pKU-ULcYmtec", {
@@ -10,16 +18,15 @@ const handlePostRequest = async () => {
     }]
    });
 
-      console.log(response.data.candidates[0].content.parts[0].text);
+      setAnswer(response.data.candidates[0].content.parts[0].text);
     } catch (error) {
       console.error("Error:", error);
     }
   };
-
-const App = () => {
   return (
     <>
 <button onClick={handlePostRequest}>send request</button>
+<p>{answer}</p>
 </>
     )
 }
