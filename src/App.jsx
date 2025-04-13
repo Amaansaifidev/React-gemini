806a733f45c0b8ffe86f74c21ff3ed6be6bed816
amaansaifidev <amaansaofidev@gmail.com>
2025-04-13 16:58:41 +0000

    tailwind5

--- src/App.jsx
+++ src/App.jsx
@@ -22,8 +22,6 @@ import { useState } from 'react'
 import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import Box from '@mui/material/Box';
-import TextField from '@mui/material/TextField';
-
 import { jwtDecode } from "jwt-decode";
 import GoogleAuth from './Components/GoogleAuth.jsx'
 
