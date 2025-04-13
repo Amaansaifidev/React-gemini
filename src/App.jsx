cd708a146e1c8f0d642baf7a084e6997e9326d14
amaansaifidev <amaansaofidev@gmail.com>
2025-04-13 16:45:11 +0000

    tailwind3

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ borderRadius:"5px",
   <ContentCopyIcon />
 } onClick={copyhandler}>copy Answer</Button>
 
-<TextField className="placeholder-red"  ref={inputref} variant="outlined" id="input" placeholder="type your msg" 
+<TextField className="placeholder-white text-white bg-black border p-2"  ref={inputref} variant="outlined" id="input" placeholder="type your msg" 
 type="text"
 onChange={handleChange}
 onKeyDown={handleKeyPress} 
