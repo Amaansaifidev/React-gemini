2e2c2236c55630089061784f49005dd75b099ef8
amaansaifidev <amaansaofidev@gmail.com>
2025-04-20 20:00:33 +0000

    about

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,26 @@
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion"
+
+
 import React from 'react'
 import { Link } from 'react-router-dom'
 const About = () => {
   return(
-  <Link to={"/"}><button>
-  go to home page
-</button>
-</Link>
+  <>
+    <Accordion type="single" collapsible>
+  <AccordionItem value="item-1">
+    <AccordionTrigger>Is it accessible?</AccordionTrigger>
+    <AccordionContent>
+      Yes. It adheres to the WAI-ARIA design pattern.
+    </AccordionContent>
+  </AccordionItem>
+</Accordion>
+
+    </>
   )
 }
 export default About;
